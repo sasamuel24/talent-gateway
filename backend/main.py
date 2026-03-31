33abd7aa467fbeb94e4988a85af5cc2a14a7da1b
sasamuel24 <122072427+sasamuel24@users.noreply.github.com>
2026-03-31 13:34:25 -0500

    cors del api del backend

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://5ia57v1jmg.execute-api.us-east-2.amazonaws.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
